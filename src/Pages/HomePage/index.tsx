2d6de9db0d6d1476b7ee9876fa665154c5c785a2
Sirak Radaa <radaa.sirak@gmail.com>
2023-05-03 20:23:39 -0600

    Added firebase and added .env to ignore

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,23 +1,39 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import TitleBarImageListComponent from './../../Components/TitleBarImageList/index';
-import CheckboxComponent from './../../Components/Checkbox/index';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import TitleBarImageListComponent from "./../../Components/TitleBarImageList/index";
+import { db } from "../../firebase";
+import { collection, doc, getDocs, onSnapshot } from "firebase/firestore";
 
+const data = async () => {
+  const dbRef = collection(db, "users");
+
+  onSnapshot(dbRef, (docsSnap) => {
+    docsSnap.forEach((doc) => {
+      console.log(doc.data());
+    });
+  });
+
+  // querySnapshot.forEach((doc) => {
+  //   // doc.data() is never undefined for query doc snapshots
+  //   console.log(doc.id, " => ", doc.data());
+  // });
+};
 
 const HomePage = () => {
+  data();
 
-    const labels = ["humid", "tropical", "dry", "cold", "hot"]
+  // const users = collection(db, "users");
+  // const docSnap = getDocs(users);
 
+  // console.log("test", docSnap);
 
-    return(
-        <><Grid container justifyContent={'center'} padding={1}>
-            <CheckboxComponent labels={labels}/>
-            <TitleBarImageListComponent/>
-            </Grid>
-        
-        </>
-    )
-
-}
+  return (
+    <>
+      <Grid container justifyContent={"center"} padding={1}>
+        <TitleBarImageListComponent />
+      </Grid>
+    </>
+  );
+};
 
 export default HomePage;
